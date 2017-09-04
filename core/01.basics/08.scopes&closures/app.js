d56b41fa35199b6a5a7bdf13733571f96210effc
monodonBrand <office@monodon.net>
2017-09-04 23:18:23 +0300

    Exercise 8

---
+++ core/01.basics/08.scopes&closures/app.js
@@ -0,0 +1,15 @@
+const globalVar = "A global variable"
+
+function localBlock() {
+  const blockVar = "A local block variable"
+}
+
+function outerFunction() {
+  const outerVar = "Outer function variable"
+  return function innerFunction() {
+    const innerVar = "Inner function variable"
+    console.log(outerVar)
+  }
+}
+
+outerFunction()()
