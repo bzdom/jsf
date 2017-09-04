e81084ce578129105307304db6a992530bbaf16b
monodonBrand <office@monodon.net>
2017-09-04 16:54:56 +0300

    Did exersize 7

---
+++ core/01.basics/07.functions/app.js
@@ -0,0 +1,18 @@
+function logger(string) {
+  console.log(string)
+}
+
+function add(num1, num2) {
+  return num1+ num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+logger('This is exercise 7')
+
+console.log(add(10, 26))
+
+const fun3 = multiply(5, 4)
+console.log(fun3)
