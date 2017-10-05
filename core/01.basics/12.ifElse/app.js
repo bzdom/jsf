f68e4579c224f6aecf0f2eba4299efc7004c921a
monodonBrand <office@monodon.net>
2017-10-05 16:33:14 +0300

    Lesson 12
    if/else statments

---
+++ core/01.basics/12.ifElse/app.js
@@ -0,0 +1,22 @@
+const james = 22
+const valerie = 25
+const kenneth = 27
+const me = 37
+
+if (me < james) {
+  console.log("Younger than James")
+} else if (me > valerie) {
+  console.log("Older than Valerie")
+} else if (me === kenneth) {
+  console.log("Ad old as Kenneth")
+} else {
+  console.log("No..")
+}
+
+const someValue = ['one', 'two', 'three']
+
+if (someValue) {
+  console.log("Yes! it is True")
+} else {
+  console.log("No.. it is False")
+}
