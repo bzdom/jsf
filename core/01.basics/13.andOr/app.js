b615b5f4e1d69d49320dabc2addaa74a1c56f94e
monodonBrand <office@monodon.net>
2017-10-09 16:41:33 +0300

    Lesson 13
    &&, || and !

---
+++ core/01.basics/13.andOr/app.js
@@ -0,0 +1,21 @@
+qOne = 'Benjamin' && 'Thaddeus'
+qTwo = 'Benjamin' || 'Thaddeus'
+qThree = '' && null
+qFour = '' || null
+qFive = 2550284 && 0
+qSix = 2550284 || 0
+qSeven = !2550284
+qEight = !true
+qNine = !NaN
+qTen = !{}
+
+console.log(qOne)
+console.log(qTwo)
+console.log(qThree)
+console.log(qFour)
+console.log(qFive)
+console.log(qSix)
+console.log(qSeven)
+console.log(qEight)
+console.log(qNine)
+console.log(qTen)
