9b2170f4f2fad5f4a8d30131fa5782e8529259a7
Shahar-Levy <office@monodon.net>
2017-12-29 18:05:40 +0200

    Lesson 6 Accordion

---
+++ core/05.building-components/06.accordion/app.js
@@ -0,0 +1,8 @@
+const accordionContainer = document.querySelector('.jsAccordionContainer')
+
+ accordionContainer.addEventListener('click', e => {
+  const header = e.target.closest('.jsAccordionHeader')
+  if(header) {
+    header.parentNode.classList.toggle('is-open')
+  }
+})
